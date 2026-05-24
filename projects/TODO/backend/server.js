a47e852fb709dce54e39cef72a7ce22850c23063
Your Real Name <vivobook.9822@gmail.com>
2026-05-24 19:38:15 +0530

    Add projects and TODO app structure

--- projects/TODO/backend/server.js
+++ projects/TODO/backend/server.js
@@ -14,7 +14,10 @@ const PORT =process.env.PORT|| 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+     origin:"https://todo-psi-seven-21.vercel.app/"
+}
+));
 
 
 
