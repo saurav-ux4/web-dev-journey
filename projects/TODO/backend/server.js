71ff0c5666904e7abe96f3c1082f45214f682c3a
Your Real Name <vivobook.9822@gmail.com>
2026-05-25 09:51:47 +0530

    updated

--- projects/TODO/backend/server.js
+++ projects/TODO/backend/server.js
@@ -15,7 +15,7 @@ const PORT =process.env.PORT|| 5000;
 // Middleware
 app.use(express.json());
 app.use(cors({
-     origin:"https://todo-psi-seven-21.vercel.app/"
+     origin:"https://todo-psi-seven-21.vercel.app"
 }
 ));
 
