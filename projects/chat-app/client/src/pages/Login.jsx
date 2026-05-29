7971f71084ee3d5c3ed54dd7435a07a9de5a87f7
Your Real Name <vivobook.9822@gmail.com>
2026-05-29 10:03:49 +0530

    complete frontend authentication flow

--- projects/chat-app/client/src/pages/Login.jsx
+++ projects/chat-app/client/src/pages/Login.jsx
@@ -1,7 +1,91 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import API from "../services/api";
+
 function Login() {
-  return (
+
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  });
+
+
+
+ const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+
+
+
+   const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+
+      const res = await API.post(
+        "/auth/login",
+        formData
+      );
+ 
+     
+      localStorage.setItem(
+        "userInfo",
+        JSON.stringify(res.data)
+      );
+  
+
+      alert("Login Successful");
+
+      navigate("/");
+
+    } catch (error) {
+
+      alert(error.response.data.message);
+
+    }
+  };
+
+
+   return (
     <div>
-      <h1>Login Page</h1>
+
+      <h1>Login</h1>
+
+      <form onSubmit={handleSubmit}>
+
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          onChange={handleChange}
+        />
+
+        <br />
+        <br />
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={handleChange}
+        />
+
+        <br />
+        <br />
+
+        <button type="submit">
+          Login
+        </button>
+
+      </form>
+
     </div>
   );
 }
--- projects/chat-app/client/src/pages/Register.jsx
+++ projects/chat-app/client/src/pages/Register.jsx
@@ -1,9 +1,109 @@
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import API from "../services/api";
+
+
+
+
 function Register() {
-  return (
+
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: ""
+  });
+
+
+
+   const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+
+
+   const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+
+      const res = await API.post(
+        "/auth/register",
+        formData
+      );
+
+      console.log(res.data);
+
+      localStorage.setItem(
+      "userInfo",
+      JSON.stringify(res.data)
+      );
+
+      navigate("/");
+
+      alert("Registration Successful");
+
+    } catch (error) {
+
+      console.log(error.response.data);
+
+      alert(error.response.data.message);
+
+    }
+  };
+
+
+    return (
     <div>
-      <h1>Register Page</h1>
+
+      <h1>Register</h1>
+
+      <form onSubmit={handleSubmit}>
+
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          onChange={handleChange}
+        />
+
+        <br />
+        <br />
+
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          onChange={handleChange}
+        />
+
+        <br />
+        <br />
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={handleChange}
+        />
+
+        <br />
+        <br />
+
+        <button type="submit">
+          Register
+        </button>
+
+      </form>
+
     </div>
   );
 }
+
+
 
 export default Register;
