37aa274e8e740624bbdf48b65e13a966eb6dc0f3
Your Real Name <vivobook.9822@gmail.com>
2026-06-07 08:58:41 +0530

    add link to server

--- projects/chat-app/client/src/context/SocketProvider.jsx
+++ projects/chat-app/client/src/context/SocketProvider.jsx
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 import { AuthContext } from "./AuthContext";
 import { SocketContext } from "./SocketContext";
 
-const socket = io("http://localhost:5000");
+const socket = io(import.meta.env.VITE_SOCKET_URL);
 
 const SocketProvider = ({ children }) => {
   const { user } = useContext(AuthContext);
